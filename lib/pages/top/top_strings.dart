c557aef107f09cac472e89090acca5fb99b73dfb
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-06-06 01:15:25 +0900

    タブと設定ページを作成

---
+++ lib/pages/top/top_strings.dart
@@ -0,0 +1,26 @@
+/*
+ * Topページで使用する文言をここで管理する
+ */
+/// 画面タイトル
+const String screenTitle = 'Qiita検索表示';
+
+/// エラーログ
+const String errorString = 'Error';
+
+/// 人気記事タブタイトル
+const String popularTabTitle = '人気記事';
+
+/// 記事検索タブタイトル
+const String searchArticleTabTitle = '記事検索';
+
+/// 設定タブタイトル
+const String settingTabTitle = '設定';
+
+/// システム設定に従う文言
+const String selectSystemMode = 'システムの設定に従う';
+
+/// ライトモード文言
+const String lightMode = 'ライトモード';
+
+/// ダークモード文言
+const String darkMode = 'ダークモード';
